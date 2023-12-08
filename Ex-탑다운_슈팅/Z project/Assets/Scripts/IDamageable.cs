fd52c9063145261fcb696f531c15ecafbd3f443c
Dry_P <80026010+ParkGeonjo@users.noreply.github.com>
2023-12-08 18:48:21 +0900

    Delete Ex-탑다운_슈팅/Z project directory

--- Ex-탑다운_슈팅/Z project/Assets/Scripts/IDamageable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDamageable // 인터페이스 생성.
-{
-    void TakeHit(float damage, Vector3 hitPoint, Vector3 hitDirection); // 타격을 받는 메소드
-    void TakeDamage(float damage); // 데미지를 받는 메소드
-}

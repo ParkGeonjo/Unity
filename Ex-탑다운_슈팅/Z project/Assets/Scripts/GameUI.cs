fb13a14c28aebea56eefe1a979d29c397bc37826
ParkGeonjo <pg3688@naver.com>
2023-12-13 17:37:51 +0900

    2023-12-13

--- Ex-탑다운_슈팅/Z project/Assets/Scripts/GameUI.cs
+++ Ex-탑다운_슈팅/Z project/Assets/Scripts/GameUI.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameUI : MonoBehaviour {
     public Image fadePlane; // 페이드 이미지(UI 배경) 오브젝트 레퍼런스
     public GameObject gameOverUI; // 게임 오버 텍스트, 버튼 오브젝트 레퍼런스
 
     public RectTransform newWaveBanner; // 배너 UI 레퍼런스
-    public Text newWaveTitle; // 새 웨이브 타이틀 텍스트 레퍼런스
-    public Text newWaveEnemyCount; // 새 웨이브 적 카운트 텍스트 레퍼런스
+    public TextMeshProUGUI newWaveTitle; // 새 웨이브 타이틀 텍스트 레퍼런스
+    public TextMeshProUGUI newWaveEnemyCount; // 새 웨이브 적 카운트 텍스트 레퍼런스
 
     Spawner spawner; // 적 스폰기 레퍼런스
 
@@ -26,9 +27,12 @@ public class GameUI : MonoBehaviour {
     void OnNewWave(int waveNumber) {
         string[] numbers = { "One", "Two", "Three", "Four", "Five" }; // 숫자 텍스트 문자열
         newWaveTitle.text = "- Wave " + numbers[waveNumber - 1] + " -"; // 새 웨이브 배너 타이틀 텍스트 설정
-        newWaveEnemyCount.text = "Enemy : " + spawner.waves[waveNumber - 1].enemyCount; // 새 웨이브 적 카운트 텍스트 설정
+        string enemyCountString = ((spawner.waves[waveNumber - 1].infinite) ? "Infinite" : spawner.waves[waveNumber - 1].enemyCount + "");
+        // 해당 웨이브가 무한 모드인지 판단, 맞다면 Infinite, 아니면 적 수를 출력한다
+        newWaveEnemyCount.text = "Enemy : " + enemyCountString; // 새 웨이브 적 카운트 텍스트 설정
 
-        StartCoroutine(AnimateNewWaveBanner()); // 새 웨이브 배너 애니메이션 코루틴
+        StopCoroutine("AnimateNewWaveBanner"); // 새 웨이브 배너 애니메이션 코루틴 중지
+        StartCoroutine("AnimateNewWaveBanner"); // 새 웨이브 배너 애니메이션 코루틴
     }
 
     // ■ 플레이어 사망(게임 오버) 시 UI 처리 메소드
@@ -43,12 +47,23 @@ public class GameUI : MonoBehaviour {
         float delayTime = 1f; // 배너 대기 시간
         float speed = 2.5f; // 배너 움직이는 시간
         float animatePercent = 0; // 애니메이션 실행 퍼센트 설정
-        int dir = 1; // 이동 방향
+        int dir = 1; // 배너 이동 방향, 양수면 위, 음수면 아래쪽
 
-        float endDelayTime = Time.time + 1 / speed + delayTime; // 배너 대기시간 설정
+        float endDelayTime = Time.time + 1 / speed + delayTime; // 배너 나타난 후 대기시간 설정
 
-        while(animatePercent < 1) { // 애니메이션 실행
+        while(animatePercent >= 0) { // 애니메이션 실행
             animatePercent += Time.deltaTime * speed * dir; // 애니메이션 실행 퍼센트 설정
+
+            if (animatePercent >= 1) { // 애니메이션 실행 퍼센트가 1 이상인 경우
+                animatePercent = 1; // 1로 설정
+                if(Time.time > endDelayTime) { // 현재 시간이 배너가 나타나고 대기시간까지 지난 경우
+                    dir = -1; // 방향을 음수로 설정, 아래로 이동하도록 함
+                }
+            }
+
+            newWaveBanner.anchoredPosition = Vector2.up * Mathf.Lerp(-190, 45, animatePercent); // 배너의 위치를 설정
+
+            yield return null; // 다음 프레임으로 스킵 
         }
         
     }
